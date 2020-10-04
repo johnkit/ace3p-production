c17a0545ed23048a7686dcbd8254a5131be327db
John Tourtellott <john.tourtellott@kitware.com>
2020-10-04 15:02:24 -0400

    Begin vuejs client for presenting test sequence

---
+++ test/pages/src/TestList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div v-for="(test, index) in tests" :key="index">
+            <a :href="test" @click.prevent="select(`${index}`)">{{test}}</a>
+        </div>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    props: {
+        tests: {
+            type: Array,
+            default: function() {return ['from prop']}
+        }
+    },
+    data: function() {
+        return {
+            // tests: [ 'example', 'setup', '01-omega3p-test1'],
+        }
+    },  // data
+    methods: {
+        select: function(i) {
+            let test = this.tests[i];
+            // console.log(`emitting test ${test}`);
+            this.$emit('selected', test)
+        }
+    },  // methods
+}
+</script>
+
+<style scoped>
+</style>
