76025b405a980ebf2ec90242913fd7cacd07c80c
John Tourtellott <john.tourtellott@kitware.com>
2020-10-14 21:51:14 -0400

    Fix indentation; in .vue files

--- docs/ui-testing/TestList.vue
+++ docs/ui-testing/TestList.vue
@@ -1,31 +1,29 @@
 <template>
-    <div>
-        <div v-for="(test, index) in tests" :key="index">
-            <a :href="test" @click.prevent="select(`${index}`)">{{test}}</a>
-        </div>
+  <div>
+    <div v-for="(test, index) in tests" :key="index">
+      <a :href="test" @click.prevent="select(`${index}`)">{{test}}</a>
     </div>
+  </div>
 </template>
 
 <script>
 module.exports = {
-    props: {
-        tests: {
-            type: Array,
-            default: function() {return ['from prop']}
-        }
-    },
-    data: function() {
-        return {
-            // tests: [ 'example', 'setup', '01-omega3p-test1'],
-        }
-    },  // data
-    methods: {
-        select: function(i) {
-            let test = this.tests[i];
-            // console.log(`emitting test ${test}`);
-            this.$emit('selected', test)
-        }
-    },  // methods
+  props: {
+    tests: {
+      type: Array,
+      default: function() {return ['from prop']}
+    }
+  },
+  data: function() {
+    return {
+    }
+  },  // data
+  methods: {
+    select: function(i) {
+      let test = this.tests[i];
+      this.$emit('selected', test)
+    }
+  },  // methods
 }
 </script>
 
